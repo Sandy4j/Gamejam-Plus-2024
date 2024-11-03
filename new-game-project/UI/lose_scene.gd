abb8d48f9259d23ac6b9f0b11a197d6a77a04d16
Tiyyasha Ananda <tiyyashaananda@gmail.com>
2024-11-03 19:57:43 +0700

    ending

---
+++ new-game-project/UI/lose_scene.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$"Credit-pop".visible = false
+	$AnimationPlayer.play("game_over")
+	await  get_tree().create_timer(5).timeout
+	$Gameover.visible = false
+	$AnimationPlayer.play("credit")
+	$"Credit-pop".visible = true
+	await  get_tree().create_timer(10).timeout
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
+		get_tree().change_scene_to_file("res://UI/main_menu.tscn")
